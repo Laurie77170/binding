5e303c975d1462597a1956c2f056db9d7726785a
Laurie77170 <[Laurie77170](mailto:lauriebou1984@gmail.com)>
2023-01-11 10:29:30 +0100

    exo

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'contact-file';
+}
